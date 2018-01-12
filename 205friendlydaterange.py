bc890ac050dd7408e5874eee7eb14742b0f37e42
Albert <albertswchung@gmail.com>
2018-01-12 11:43:54 +0000

    More Daily Programmer Exercises

---
+++ 205friendlydaterange.py
@@ -0,0 +1,11 @@
+import datetime
+
+def friendly_date(dates):
+    dates = [x for x in dates.split(' ')]
+    dates = 
+
+
+
+if __name__ == "__main__":
+    input_1 = '2015-07-01 2015-07-04'
+    friendly_date(input_1)
